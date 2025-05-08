4a4a887c9ee349b46874581f12245b8835297267
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-05-08 22:31:42 +0530

    Backend code for user registration

---
+++ server/src/main/java/com/example/server/User/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.server.User.controller;
+
+import com.example.server.User.entity.User;
+import com.example.server.User.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+}
